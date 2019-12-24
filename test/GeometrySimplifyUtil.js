ba39e6985f4c14a5decfbdc3bc23fd4cfa7aeb11
LeonYuanYao <yaoyuan@suitbim.com>
2019-12-24 18:55:42 +0800

    change codes in geometrySimplifyUtil

--- test/GeometrySimplifyUtil.js
+++ test/GeometrySimplifyUtil.js
@@ -62,15 +62,23 @@ var Face = function (index1, index2, index3, v1, v2, v3, n1, n2, n3, uv1, uv2, u
 
 // 
 
-
 var GeometrySimplifyUtil = function () {
 
     let scope = this;
+
     this.source = null;
     this.newGeom = new THREE.Geometry();
     this.box = new THREE.Box3();
     this.facelist = [];
-    this.boxlist = [];
+
+    /**
+     * Map <boxid, box>
+     * eg. boxid: "0_2_1", box: { vertices: [], point: [] }
+     */
+    this.boxmap = new Map();
+    this.xcomp = 0;
+    this.ycomp = 0;
+    this.zcomp = 0;
 
 
     function updateBox() {
@@ -80,80 +88,88 @@ var GeometrySimplifyUtil = function () {
 
 
     function parseGeometry() {
-        let index = scope.source.index;
-        let vertices = scope.source.attributes.position;
-        let normals = scope.source.attributes.normal;
-        let uvs = scope.source.attributes.uv;
 
-        if (!index) {
-            // has no index in geometry
-            let face;
-            for (let i = 0; i < vertices.count; i++) {
+        let faces, vertices, uvs;
 
-                let vertex = new THREE.Vector3(vertices.array[i * 3 + 0], vertices.array[i * 3 + 1], vertices.array[i * 3 + 2]);
-                let normal = new THREE.Vector3(normals.array[i * 3 + 0], normals.array[i * 3 + 1], normals.array[i * 3 + 2]);
-                let uv = new THREE.Vector2(uvs.array[i * 2 + 0], uvs.array[i * 2 + 1]);
+        if (scope.source instanceof THREE.Geometry) {
 
-                if (i % 3 == 0) {
-                    face = new Face(i + 0, i + 1, i + 2);
-                    scope.facelist.push(face);
-                }
+            faces = scope.source.faces;
+            vertices = scope.source.vertices;
+            uvs = scope.source.faceVertexUvs;
 
-                face.vertices.push(vertex);
-                face.normals.push(normal);
-                face.uvs.push(uv);
-            }
+        } else if (scope.source instanceof THREE.BufferGeometry) {
+
+            scope.source = new THREE.Geometry().fromBufferGeometry(scope.source);
+            faces = scope.source.faces;
+            vertices = scope.source.vertices;
+            uvs = scope.source.faceVertexUvs;
 
         } else {
-            // already has index
+
+            throw ("The geometry to be simplified is none of 'THREE.Geometry' or 'THREE.BufferGeometry'. ");
 
         }
+
+
+        let newFace;
+        for (let i = 0; i < faces.length; i++) {
+
+            let face = faces[i];
+
+            newFace = new Face(face.a, face.b, face.c);
+            scope.facelist.push(face);
+
+            newFace.setVertices(vertices[face.a].clone(), vertices[face.b].clone(), vertices[face.c].clone());
+            
+            if (uvs.length > 0){
+                newFace.setUvs()
+            }
+
+
+            let vertex = new THREE.Vector3(vertices.array[i * 3 + 0], vertices.array[i * 3 + 1], vertices.array[i * 3 + 2]);
+            let normal = new THREE.Vector3(normals.array[i * 3 + 0], normals.array[i * 3 + 1], normals.array[i * 3 + 2]);
+            let uv = new THREE.Vector2(uvs.array[i * 2 + 0], uvs.array[i * 2 + 1]);
+
+            face.vertices.push(vertex);
+            face.normals.push(normal);
+            face.uvs.push(uv);
+        }
+
+
 
         updateBox();
     }
 
 
-    function splitBox(segments) {
-        scope.boxlist.length = 0;
+    function initBoxInfo(segments) {
+        scope.boxmap = new Map();
         let size = new THREE.Vector3();
         scope.box.getSize(size);
-        let xsize = size.x / segments;
-        let ysize = size.y / segments;
-        let zsize = size.z / segments;
-        let min = scope.box.min;
-        for (let i = 0; i < segments; i++) {
-            for (let j = 0; j < segments; j++) {
-                for (let k = 0; k < segments; k++) {
-                    let box = new THREE.Box3(
-                        new THREE.Vector3(min.x + i * xsize, min.y + j * ysize, min.z + k * zsize),
-                        new THREE.Vector3(min.x + (i + 1) * xsize, min.y + (j + 1) * ysize, min.z + (k + 1) * zsize)
-                    );
-                    box.id = `${i}_${j}_${k}`;
-                    box.vertices = [];
-                    box.point = null;
-                    scope.boxlist.push(box);
-                }
-            }
-        }
+        scope.xcomp = size.x / segments;
+        scope.ycomp = size.y / segments;
+        scope.zcomp = size.z / segments;
     }
 
 
     function computeVertexBox(vertex) {
-        for (let i = 0; i < scope.boxlist.length; i++) {
-            let box = scope.boxlist[i];
-            if (box.containsPoint(vertex)) {
-                vertex.box = box;
-                box.vertices.push(vertex);
-                return;
-            }
+        let x = parseInt((vertex.x - scope.box.min.x) / scope.xcomp);
+        let y = parseInt((vertex.y - scope.box.min.y) / scope.ycomp);
+        let z = parseInt((vertex.z - scope.box.min.z) / scope.zcomp);
+        let boxid = `${x}_${y}_${z}`;
+        vertex.boxid = boxid;
+
+        if (scope.boxmap.has(boxid)) {
+            scope.boxmap.get(boxid).vertices.push(vertex);
+        } else {
+            scope.boxmap.set(boxid, {
+                vertices: [vertex],
+                point: null,
+            });
         }
     }
 
 
     function computeBoxAvgPoint(box) {
-        if (box.vertices.length == 0)
-            return;
-
         let sumx = 0;
         let sumy = 0;
         let sumz = 0;
@@ -168,13 +184,12 @@ var GeometrySimplifyUtil = function () {
     }
 
 
-    function checkDegeneration(face) {
+    function checkFaceDegen(face) {
         let a = face.vertices[0];
         let b = face.vertices[1];
         let c = face.vertices[2];
-        if (a.box == b.box || a.box == c.box || b.box == c.box) {
-            // console.log("found degenerated face!")
-            // console.log(a, b, c)
+        if (a.boxid == b.boxid || a.boxid == c.boxid || b.boxid == c.boxid) {
+            face.degenerated = true;
             return true;
         }
         return false;
@@ -184,11 +199,16 @@ var GeometrySimplifyUtil = function () {
     this.simplify = function (geometry, params) {
         this.source = geometry;
         let segments = params.segments;
-        let newGeom = new THREE.BufferGeometry();
+        let newBufGeom = new THREE.BufferGeometry();
+
+        if (segments < 2) {
+            console.error("The param.segments cannot be smaller than 2");
+            return;
+        }
 
         parseGeometry();
 
-        splitBox(segments);
+        initBoxInfo(segments);
 
         // determine every vertex's box & check face's degeneration
         for (let i = 0; i < this.facelist.length; i++) {
@@ -199,45 +219,37 @@ var GeometrySimplifyUtil = function () {
                 let vertex = face.vertices[j];
                 computeVertexBox(vertex);
             }
-        }
 
-        var originFaces = this.facelist.length;
-        console.log("facelist.length", originFaces)
-
-        for (let i = 0; i < this.facelist.length; i++) {
-            let face = this.facelist[i];
-            // check if the face needs to be degenerated
-            if (checkDegeneration(face)) {
-                this.facelist.splice(this.facelist.indexOf(face), 1);
-            }
+            checkFaceDegen(face);
         }
 
         console.log("facelist.length", this.facelist.length)
-        alert(`Face reduced: ${originFaces - this.facelist.length}`);
+        this.facelist = this.facelist.filter(f => !f.degenerated);
+        console.log("facelist.length", this.facelist.length)
 
-
-        // set every vertex in box to be the adjusted-point-value
-        for (let i = 0; i < this.boxlist.length; i++) {
-            let box = this.boxlist[i];
+        // set every vertex to be the box-point-value
+        scope.boxmap.forEach((box) => {
             if (!box.point) {
                 computeBoxAvgPoint(box);
             }
 
-            for (let j = 0; j < box.vertices.length; j++) {
-                box.vertices[j].set(box.point.x, box.point.y, box.point.z);
+            for (let i = 0; i < box.vertices.length; i++) {
+                box.vertices[i].set(box.point.x, box.point.y, box.point.z);
             }
-        }
-
+        });
 
         if (!scope.source.index) {
             // no index in source geom
-
             let newVertices = new Float32Array(this.facelist.length * 3 * 3);
-            let newNormals = new Float32Array(this.facelist.length * 3 * 3);
-            let newUvs = new Float32Array(this.facelist.length * 3 * 3);
+            // let newNormals = new Float32Array(this.facelist.length * 3 * 3);
+            // let newUvs = new Float32Array(this.facelist.length * 3 * 3);
 
             for (let i = 0; i < this.facelist.length; i++) {
                 let face = this.facelist[i];
+
+                if (face.degenerated) {
+                    continue; // skip when face needs degenerate
+                }
 
                 newVertices[i * 9 + 0] = face.vertices[0].x;
                 newVertices[i * 9 + 1] = face.vertices[0].y;
@@ -249,32 +261,37 @@ var GeometrySimplifyUtil = function () {
                 newVertices[i * 9 + 7] = face.vertices[2].y;
                 newVertices[i * 9 + 8] = face.vertices[2].z;
 
-                newNormals[i * 9 + 0] = face.normals[0].x;
-                newNormals[i * 9 + 1] = face.normals[0].y;
-                newNormals[i * 9 + 2] = face.normals[0].z;
-                newNormals[i * 9 + 3] = face.normals[1].x;
-                newNormals[i * 9 + 4] = face.normals[1].y;
-                newNormals[i * 9 + 5] = face.normals[1].z;
-                newNormals[i * 9 + 6] = face.normals[2].x;
-                newNormals[i * 9 + 7] = face.normals[2].y;
-                newNormals[i * 9 + 8] = face.normals[2].z;
+                // newNormals[i * 9 + 0] = face.normals[0].x;
+                // newNormals[i * 9 + 1] = face.normals[0].y;
+                // newNormals[i * 9 + 2] = face.normals[0].z;
+                // newNormals[i * 9 + 3] = face.normals[1].x;
+                // newNormals[i * 9 + 4] = face.normals[1].y;
+                // newNormals[i * 9 + 5] = face.normals[1].z;
+                // newNormals[i * 9 + 6] = face.normals[2].x;
+                // newNormals[i * 9 + 7] = face.normals[2].y;
+                // newNormals[i * 9 + 8] = face.normals[2].z;
 
-                newUvs[i * 6 + 0] = face.uvs[0].x;
-                newUvs[i * 6 + 1] = face.uvs[0].y;
-                newUvs[i * 6 + 2] = face.uvs[1].x;
-                newUvs[i * 6 + 3] = face.uvs[1].y;
-                newUvs[i * 6 + 4] = face.uvs[2].x;
-                newUvs[i * 6 + 5] = face.uvs[2].y;
+                // newUvs[i * 6 + 0] = face.uvs[0].x;
+                // newUvs[i * 6 + 1] = face.uvs[0].y;
+                // newUvs[i * 6 + 2] = face.uvs[1].x;
+                // newUvs[i * 6 + 3] = face.uvs[1].y;
+                // newUvs[i * 6 + 4] = face.uvs[2].x;
+                // newUvs[i * 6 + 5] = face.uvs[2].y;
+
             }
 
-            newGeom.setAttribute('position', new THREE.BufferAttribute(newVertices, 3));
-            newGeom.setAttribute('normal', new THREE.BufferAttribute(newNormals, 3));
-            newGeom.setAttribute('uv', new THREE.BufferAttribute(newUvs, 3));
+            newBufGeom.setAttribute('position', new THREE.BufferAttribute(newVertices, 3));
+            // newGeom.setAttribute('normal', new THREE.BufferAttribute(newNormals, 3));
+            // newGeom.setAttribute('uv', new THREE.BufferAttribute(newUvs, 3));
 
-            newGeom.computeFaceNormals();
+            var newGeom = new THREE.Geometry();
+            newGeom.fromBufferGeometry(newBufGeom);
+
+            newGeom.computeVertexNormals();
             newGeom.computeBoundingBox();
 
         } else {
+            // has index
 
         }
 
